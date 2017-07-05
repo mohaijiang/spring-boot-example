1e66b3950f93008b303199dde54a0950f2ce5e3b
mohaijiang <haijiang.mo@newtouch.cn>
2017-07-05 11:30:27 +0800

    first commit

---
+++ src/main/java/cn/newtouch/mohaijiang/example/SampleController.java
@@ -0,0 +1,26 @@
+package cn.newtouch.mohaijiang.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 启动类
+ * Created by mohaijiang on 17-7-5.
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
