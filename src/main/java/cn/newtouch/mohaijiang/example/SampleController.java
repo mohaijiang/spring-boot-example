860c8d2bfbbe5efa0526ee638df5f5833531e85d
mohaijiang <haijiang.mo@newtouch.cn>
2018-03-07 18:21:18 +0800

    update show number

--- src/main/java/cn/newtouch/mohaijiang/example/SampleController.java
+++ src/main/java/cn/newtouch/mohaijiang/example/SampleController.java
@@ -20,7 +20,7 @@ public class SampleController  extends SpringBootServletInitializer {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World version 323 !";
+        return "Hello World version 326 !";
     }
 
     public static void main(String[] args) {
