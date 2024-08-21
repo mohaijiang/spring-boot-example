6bb334a92a3c3dff66306dcdc4b1f1a9a16876ff
mohaijiang <mohaijiang110@163.com>
2024-08-21 15:20:35 +0800

    增加actuator

--- src/main/java/cn/newtouch/mohaijiang/example/DemoApplication.java
+++ src/main/java/cn/newtouch/mohaijiang/example/DemoApplication.java
@@ -1,7 +1,11 @@
 package cn.newtouch.mohaijiang.example;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +30,9 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
 }
